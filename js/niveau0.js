8864523af745945fafe08dae12bf1f2022ee150f
Spoonyz <32356544+Spoonyz@users.noreply.github.com>
2017-10-17 18:31:27 +0200

    Coucou :)

---
+++ js/niveau0.js
@@ -0,0 +1,13 @@
+Jeu.niveau0 = function(){};
+
+Jeu.niveau0.prototype.create = function() {
+  // Ici on définit ce que contiendra le level et on initialise toutes les variables
+}
+
+Jeu.niveau0.prototype.init = function(/* variables */){
+  // Ici on initialise les variables qui sont passé d'un état à un autre, example: le score en cours
+}
+
+Jeu.niveau0.prototype.update = function(){
+  // Cette fonction sera éxecutée en boucle, c'est donc un bon endroit pour y écrire la logique du genre "vérifier les collisions"
+}
